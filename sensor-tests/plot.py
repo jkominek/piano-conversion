d20d4bdf75c5e823c04430982f868293f09386ab
Davide Del Vento <davide.del.vento@gmail>
2021-12-27 11:36:42 -0700

    generate plot

---
+++ sensor-tests/plot.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#plt.style.use('fivethirtyeight')
+#plt.xkcd() 
+
+s1 = np.loadtxt("test.txt", usecols=0).flatten()
+s2 = np.loadtxt("test.txt", usecols=1).flatten()
+s3 = np.loadtxt("test.txt", usecols=2).flatten()
+
+dt = 0.01 
+t = dt * np.arange(len(s1))
+
+fig, axs = plt.subplots(2, 1)
+axs[0].plot(t, s1, t, s2, t, s3)
+axs[0].set_xlim(0, t[-1])
+axs[0].set_xlabel('time')
+axs[0].set_ylabel('signals')
+axs[0].grid(True)
+
+#cxy, f = axs[1].cohere(s1, s2, 256, 1. / dt)
+axs[1].set_ylabel('coherence')
+
+fig.tight_layout()
+plt.savefig("a.png", dpi=300)
