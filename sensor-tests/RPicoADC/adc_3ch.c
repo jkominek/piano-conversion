822e7618839f08c3b45036a18eea6207c664e4be
Davide Del Vento <davide.del.vento@gmail>
2021-12-27 12:33:50 -0700

    simple 3-channel adc capture in a loop

---
+++ sensor-tests/RPicoADC/adc_3ch.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+
+int main() {
+    stdio_init_all();
+    adc_init();
+    adc_gpio_init(26);
+    adc_gpio_init(27);
+    adc_gpio_init(28);
+    adc_select_input(0);
+    // 0b0001 is 26
+    // 0b0010 is 27
+    // 0b0100 is 28
+    // 0b0111 = 0x07 is all of them 
+    adc_set_round_robin(7);
+
+    int interval = 500;
+    printf("Interval of %dms", interval);
+    uint16_t r1, r2, r3;
+    while (1) {
+        r1 = adc_read();
+        r2 = adc_read();
+        r3 = adc_read();
+        printf("0x%03x 0x%03x 0x%03x\n", r1, r2, r3);
+        sleep_ms(interval);
+    }
+}
