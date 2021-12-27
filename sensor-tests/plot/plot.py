cf0ca26b2617d92c3c1986ccf066877f33e3b3c2
Davide Del Vento <davide.del.vento@gmail>
2021-12-27 14:50:19 -0700

    label placement and correlation x axis

--- sensor-tests/plot/plot.py
+++ sensor-tests/plot/plot.py
@@ -20,7 +20,7 @@ t = dt * np.arange(len(s1))
 
 fig, axs = plt.subplots(2, 1)
 axs[0].plot(t, s1, t, s2, t, s3)
-axs[0].legend(["CNY70", "QRE1113", "EAITRCA6"])
+axs[0].legend(["CNY70", "QRE1113", "EAITRCA6"], ncol=1, bbox_to_anchor=(1,1), loc="upper left")
 axs[0].set_xlim(0, t[-1])
 axs[0].set_xlabel('time')
 axs[0].set_ylim(bottom=0)
@@ -28,7 +28,11 @@ axs[0].set_ylabel('signals')
 axs[0].grid(True)
 
 try:
-    cxy, f = axs[1].cohere(s1, s2, 256, 1. / dt)
+    cxy1, f1 = axs[1].cohere(s1, s2, 256, 1. / dt, label="\nCNY70\nQRE1113\n")
+    cxy2, f2 = axs[1].cohere(s1, s3, 256, 1. / dt, label="\nCNY70\nEAITRCA6\n")
+    cxy3, f3 = axs[1].cohere(s2, s3, 256, 1. / dt, label="\nQRE1113\nEAITRCA6\n")
+    axs[1].set_xlim(0,2)
+    axs[1].legend(ncol=1, bbox_to_anchor=(1,1), loc="upper left")
 except ValueError:
     pass
 axs[1].set_ylabel('coherence')
