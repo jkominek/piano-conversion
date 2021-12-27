efa917f0c8e4591615b384f1ef32d4ab2b375a46
Davide Del Vento <davide.del.vento@gmail>
2021-12-27 13:10:49 -0700

    same timing as in the Pi (could be collected from file), range of y axis, correlation

--- sensor-tests/plot.py
+++ sensor-tests/plot.py
@@ -8,17 +8,18 @@ s1 = np.loadtxt("test.txt", usecols=0).flatten()
 s2 = np.loadtxt("test.txt", usecols=1).flatten()
 s3 = np.loadtxt("test.txt", usecols=2).flatten()
 
-dt = 0.01 
+dt = 0.05 
 t = dt * np.arange(len(s1))
 
 fig, axs = plt.subplots(2, 1)
 axs[0].plot(t, s1, t, s2, t, s3)
 axs[0].set_xlim(0, t[-1])
 axs[0].set_xlabel('time')
+axs[0].set_ylim(bottom=0)
 axs[0].set_ylabel('signals')
 axs[0].grid(True)
 
-#cxy, f = axs[1].cohere(s1, s2, 256, 1. / dt)
+cxy, f = axs[1].cohere(s1, s2, 256, 1. / dt)
 axs[1].set_ylabel('coherence')
 
 fig.tight_layout()
