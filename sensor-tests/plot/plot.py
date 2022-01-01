fdf9743f6ae1b00dd7372e4bd130e9bc9388d621
Davide Del Vento <davide.del.vento@gmail>
2022-01-01 11:39:59 -0700

    preparing for plotting 9 sensors (available now as dummy data)

--- sensor-tests/plot/plot.py
+++ sensor-tests/plot/plot.py
@@ -41,31 +41,38 @@ def plot(args):
     dt, KEY_SENSORS = parse_prefix(f.readline().replace("#", " ").strip())
     f.close()
 
-    print("hardcoding 3 ADCs instead of what is in the file")
-    s1 = np.loadtxt(args.load, usecols=0).flatten()
-    s2 = np.loadtxt(args.load, usecols=1).flatten()
-    s3 = np.loadtxt(args.load, usecols=2).flatten()
+    s = []
+    for i in range(KEY_SENSORS):
+        s.append(np.loadtxt(args.load, usecols=i).flatten())
 
-    t = dt * np.arange(len(s1))
+    t = dt * np.arange(len(s[0]))
 
-    fig, axs = plt.subplots(2, 1)
-    axs[0].plot(t, s1, t, s2, t, s3)
-    axs[0].legend(["CNY70", "QRE1113", "EAITRCA6"], ncol=1, bbox_to_anchor=(1,1), loc="upper left")
+    fig, axs = 1,1
+    if KEY_SENSORS == 3:
+        fig, axs = plt.subplots(2, 1)
+    else:
+        fig, ax_one = plt.subplots(1, 1)
+        axs = [ax_one]
+
+    for i in range(KEY_SENSORS):
+        axs[0].plot(t, s[i])
     axs[0].set_xlim(0, t[-1])
     axs[0].set_xlabel('time')
     axs[0].set_ylim(bottom=0)
     axs[0].set_ylabel('signals')
     axs[0].grid(True)
 
-    try:
-        cxy1, f1 = axs[1].cohere(s1, s2, 256, 1. / dt, label="\nCNY70\nQRE1113\n")
-        cxy2, f2 = axs[1].cohere(s1, s3, 256, 1. / dt, label="\nCNY70\nEAITRCA6\n")
-        cxy3, f3 = axs[1].cohere(s2, s3, 256, 1. / dt, label="\nQRE1113\nEAITRCA6\n")
-        axs[1].set_xlim(0,2)
-        axs[1].legend(ncol=1, bbox_to_anchor=(1,1), loc="upper left")
-    except ValueError:
-        pass
-    axs[1].set_ylabel('coherence')
+    if KEY_SENSORS == 3:
+        axs[0].legend(["CNY70", "QRE1113", "EAITRCA6"], ncol=1, bbox_to_anchor=(1,1), loc="upper left")
+        try:
+            cxy1, f1 = axs[1].cohere(s[0], s[1], 256, 1. / dt, label="\nCNY70\nQRE1113\n")
+            cxy2, f2 = axs[1].cohere(s[0], s[2], 256, 1. / dt, label="\nCNY70\nEAITRCA6\n")
+            cxy3, f3 = axs[1].cohere(s[1], s[2], 256, 1. / dt, label="\nQRE1113\nEAITRCA6\n")
+            axs[1].set_xlim(0,2)
+            axs[1].legend(ncol=1, bbox_to_anchor=(1,1), loc="upper left")
+        except ValueError:
+            pass
+        axs[1].set_ylabel('coherence')
 
     fig.tight_layout()
 
