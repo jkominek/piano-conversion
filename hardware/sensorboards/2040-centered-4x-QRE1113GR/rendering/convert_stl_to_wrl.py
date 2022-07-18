3d1c45d00be528209ec7cb0afcfde04c92dd2969
Davide Del Vento <davide.del.vento@gmail>
2022-07-17 22:06:39 -0600

    programmatically setting the scale per KiCad bug https://github.com/KiCad/kicad-packages3D/issues/405

--- hardware/sensorboards/2040-centered-4x-QRE1113GR/rendering/convert_stl_to_wrl.py
+++ hardware/sensorboards/2040-centered-4x-QRE1113GR/rendering/convert_stl_to_wrl.py
@@ -4,5 +4,11 @@ import FreeCAD
 import Mesh
 Mesh.open("./QRE1113-screen.stl")
 
-obj = FreeCAD.getDocument("Unnamed").getObject("QRE1113_screen")
-obj.Mesh.write("QRE1113-screen.wrl")
+mesh = FreeCAD.getDocument("Unnamed").getObject("QRE1113_screen").Mesh.copy()
+
+matrix = App.Matrix()
+factor = 1 / 2.54                           # Grrrrrrr: https://github.com/KiCad/kicad-packages3D/issues/405
+matrix.scale(factor,factor,factor)
+mesh.transform(matrix)
+
+mesh.write("QRE1113-screen.wrl")
